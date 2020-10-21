be461d083ffb888527e52971f18781d5ed124e7d
WNP78 <wnp78g@gmail.com>
2020-10-21 16:47:03 +0100

    Add code

---
+++ Mod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace WNP78
+{
+    public class UnityLogs : MelonMod
+    {
+        Application.LogCallback callback;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+
+            callback = callback ?? new Action<string, string, LogType>(Log);
+
+            Application.add_logMessageReceived(callback);
+        }
+
+        public override void OnApplicationQuit()
+        {
+            base.OnApplicationQuit();
+
+            Application.remove_logMessageReceived(callback);
+        }
+
+        private void Log(string condition, string stackTrace, LogType type)
+        {
+            var s = condition + "\n" + stackTrace;
+
+            switch (type)
+            {
+                case LogType.Error:
+                case LogType.Exception:
+                    MelonLogger.LogError(s);
+                    break;
+                case LogType.Warning:
+                case LogType.Assert:
+                    MelonLogger.LogWarning(s);
+                    break;
+                default:
+                    MelonLogger.Log(s);
+                    break;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnityLogs")]
+[assembly: AssemblyDescription("Shows boneworks unity logs in melonloader console.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WNP78")]
+[assembly: AssemblyProduct("UnityLogs")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c3681b9-5d67-4519-9b8d-155e42450ac2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonGame("Stress Level Zero", "BONEWORKS")]
+[assembly: MelonInfo(typeof(WNP78.UnityLogs), "UnityLogs", "1.0.0", "WNP78")]
